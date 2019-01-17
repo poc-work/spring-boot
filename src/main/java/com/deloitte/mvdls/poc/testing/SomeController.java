bdc5250204df07d696fd2bb7400038fad92affc3
Shyam Raman <shyraman@deloitte.com>
2019-01-17 16:34:34 -0500

    initial commit

---
+++ src/main/java/com/deloitte/mvdls/poc/testing/SomeController.java
@@ -0,0 +1,19 @@
+package com.deloitte.mvdls.poc.testing;
+/*
+  TITLE_API_URL: '"http://localhost:8012"',
+  ADMIN_API_URL: '"http://localhost:8001"',
+  REFERENCE_API_URL: '"http://localhost:8011"',
+  CONVERSATION_API_URL: '"http://localhost:8002"',
+  FINALIST_API_URL: '"http://localhost:8007"',
+  FEE_CALC_API_URL: '"http://localhost:8006/feeCalc"',
+  DAS_API_URL: '"http://localhost:8005"',
+  SUSPEND_WID_API_URL: '"http://localhost:8014/api/v1"',
+  SUSPEND_TRANSACTION_URL: '"http://localhost:8014"',
+  INVENTORY_API_URL: '"http://localhost:8008"',
+  CUSTOMER_API_URL: '"http://localhost:8004"',
+  VEHICLE_API_URL:'"http://localhost:8013"',
+ */
+public class SomeController {
+	
+
+}
